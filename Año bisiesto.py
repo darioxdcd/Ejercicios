4d6afe4c9691bb886e03c7c612908d53e35a74e9
Darío García <33682801+darioxdcd@users.noreply.github.com>
2017-11-17 10:57:54 +0100

    Add files via upload

---
+++ Año bisiesto.py
@@ -0,0 +1,12 @@
+print('Cual es el año?')
+año = int(input())
+if (año % 4 == 0) and (año % 100 != 0):
+  print ( 'Es bisiesto')
+elif (año % 400 == 0):
+  print('Es bisiesto')
+else:
+  print('No es bisiesto')
+
+
+   
+
