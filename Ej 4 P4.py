c2b3ebf10de626d20b453fa16035bc18afb4c6c4
Darío García <33682801+darioxdcd@users.noreply.github.com>
2018-01-31 19:30:08 +0100

    Add files via upload

---
+++ Ej 4 P4.py
@@ -0,0 +1,8 @@
+suma = 0
+print('Cual es el primer numero?')
+A = int(input()) 
+print ('Hasta que numero?')
+B=int(input())
+for i in range (A, B +1):
+  suma = suma + i
+print(suma)
---
+++ Ej5 P4.py
@@ -0,0 +1,6 @@
+print('Cual es el numero?')
+A= int(input())
+cubo= 1**3 + 2**3+A**3
+print(cubo)
+
+#No entiendo muy bien el enunciado, lo he hecho como he entendido.
